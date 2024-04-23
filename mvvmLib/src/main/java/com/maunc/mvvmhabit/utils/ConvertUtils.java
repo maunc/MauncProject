beaeaae7169f8e7022ef25ea166ae6915c2d338a
Time Will Remember Us <h16631031410@163.com>
2024-04-23 21:15:19 +0800

    1.加密方法

--- mvvmLib/src/main/java/com/maunc/mvvmhabit/utils/ConvertUtils.java
+++ mvvmLib/src/main/java/com/maunc/mvvmhabit/utils/ConvertUtils.java
@@ -130,36 +130,4 @@ public final class ConvertUtils {
         }
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
-
-    /**
-     * byte[]数组转换为16进制的字符串
-     */
-    @NonNull
-    public static String byteArray2HexString(@NonNull byte[] data) {
-        StringBuilder sb = new StringBuilder(data.length * 2);
-        for (byte b : data) {
-            int v = b & 0xff;
-            if (v < 16) {
-                sb.append('0');
-            }
-            sb.append(Integer.toHexString(v));
-        }
-        return sb.toString().toUpperCase(Locale.getDefault());
-    }
-
-    /**
-     * 16进制表示的字符串转换为字节数组
-     */
-    @NonNull
-    public static byte[] hexString2ByteArray(@NonNull String s) {
-        int len = s.length();
-        byte[] d = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            // 两位一组，表示一个字节,把这样表示的16进制字符串，还原成一个进制字节
-            d[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
-                    .digit(s.charAt(i + 1), 16));
-        }
-        return d;
-    }
-
 }
